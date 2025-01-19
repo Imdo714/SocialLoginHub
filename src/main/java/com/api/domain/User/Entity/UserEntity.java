60a0935d8bed8bd8d32416a253bf2cce1b0b347c
Imdo714 <gusehdla@naver.com>
2025-01-19 15:07:10 +0900

    CHORE 관계연산자 수정

--- src/main/java/com/api/domain/User/Entity/UserEntity.java
+++ src/main/java/com/api/domain/User/Entity/UserEntity.java
@@ -28,6 +28,12 @@ public class UserEntity {
      *  @OneToMany : 1 : N 관계  하나의 부모 여러개의 자식이있으면 부모 엔티티에 설정
      *  @ManyToOne : N : 1 관계  여러개의 자식 하나의 부모가있으면 자식 엔티티에 설정
      *  @ManyToMany : N : M 관계
+     *
+     *  mappedBy는 JPA에서 양방향 관계를 설정할 때 사용하는 속성으로, 관계의 주인을 명시합니다.
+     *  주인(Owner): 데이터베이스에서 외래 키를 관리하는 엔티티.
+     *  종속자(Inverse): 외래 키의 설정을 주인에게 위임하는 엔티티. mappedBy로 주인을 지정.
+     *
+     *  orphanRemoval은 부모-자식 관계에서 고아 객체(orphan)를 자동으로 삭제하는 기능입니다.
      */
     @OneToOne(mappedBy = "user", cascade = CascadeType.REMOVE, orphanRemoval = true) // 양방향 관계
     private TokenEntity tokenEntity;
